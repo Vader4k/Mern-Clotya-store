271802f015e64ffad2725c60a4ad389723396561
danie odukwe <kingdanny295@gmail.com>
2024-06-20 12:45:00 +0100

    @fix/ removed password validation from userSchema

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -2,50 +2,40 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     email: { 
-            type: String, 
-            required: true, 
-            unique: true,
-            trim: true,
-            match: [/.+@.+\..+/, 'Please enter a valid email address'] 
+        type: String, 
+        required: true, 
+        unique: true,
+        trim: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address'] 
     },
     Username: {
-            type: String, 
-            required: true,
-            trim: true,
-            minlength: 3,
-            unique: true,
+        type: String,
+        trim: true,
+        minlength: 3,
+        unique: true,
     },
     password: {
         type: String,
         required: true,
         minlength: 5,
         trim: true,
-        validate: {
-            validator: function(value) {
-                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value);
-            },
-            message: props => `${props.value} is not a valid password`
-        }
     },
     firstName: {
         type: String,
-        required: true,
         trim: true
       },
     lastName: {
         type: String,
-        required: true,
         trim: true
     },
     address: {
-        street: { type: String, required: true },
-        city: { type: String, required: true },
-        state: { type: String, required: true },
-        zip: { type: String, required: true }
+        street: { type: String },
+        city: { type: String },
+        state: { type: String },
+        zip: { type: String }
     },
     phone: {
         type: String,
-        required: true,
         match: [/^\d{10}$/, 'Please enter a valid 10-digit phone number']
     },
     role: {
