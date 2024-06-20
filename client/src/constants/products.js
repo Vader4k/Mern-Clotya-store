43283d506a3c686f5eab584a8ad856b620863390
danie odukwe <kingdanny295@gmail.com>
2024-06-20 18:05:03 +0100

    @add/ imported all images with a variable name

---
+++ client/src/constants/products.js
@@ -0,0 +1,357 @@
+import basic1 from '../assets/1.jpeg'
+import basic2 from '../assets/2.jpeg'
+import basic3 from '../assets/3.jpeg'
+import basic4 from '../assets/4.jpeg'
+
+import highNeck  from '../assets/basic1.jpeg'
+import highNeck2 from '../assets/basic2.jpeg'
+import highNeck3 from '../assets/basic3.jpeg'
+import highNeck4 from '../assets/basic4.jpeg'
+
+import arizona1 from '../assets/arizona1.jpeg'
+import arizona2 from '../assets/arizona2.jpeg'
+import arizona3 from '../assets/arizona3.jpeg'
+import arizona4 from '../assets/arizona4.jpeg'
+
+import asym1 from '../assets/asym1.jpeg'
+import asym2 from '../assets/asym2.jpeg'
+import asym3 from '../assets/asym3.jpeg'
+import asym4 from '../assets/asym4.jpeg'
+
+import belted1 from '../assets/belted1.jpg'
+import belted2 from '../assets/belted2.jpg'
+import belted3 from '../assets/belted3.jpg'
+import belted4 from '../assets/belted4.jpg'
+
+import blazer1 from '../assets/blazer1.jpg'
+import blazer2 from '../assets/blazer2.jpg'
+import blazer3 from '../assets/blazer3.jpg'
+import blazer4 from '../assets/blazer4.jpg'
+
+import blue1 from '../assets/blue1.jpeg'
+import blue2 from '../assets/blue2.jpeg'
+import blue3 from '../assets/blue3.jpeg'
+
+import bomber1 from '../assets/bomber1.jpg'
+import bomber2 from '../assets/bomber2.jpg'
+import bomber3 from '../assets/bomber3.jpg'
+import bomber4 from '../assets/bomber4.jpg'
+
+import satin1 from '../assets/bomber1-1.jpg'
+import satin2 from '../assets/bomber2-2.jpg'
+import satin3 from '../assets/bomber3-3.jpg'
+import satin4 from '../assets/bomber4-4.jpg'
+
+import boots1 from '../assets/boots1.jpeg'
+import boots2 from '../assets/boots2.jpeg'
+import boots3 from '../assets/boots3.jpeg'
+import boots4 from '../assets/boots4.jpeg'
+
+import breasted1 from '../assets/breasted.jpeg'
+import breasted2 from '../assets/breasted2.jpeg'
+import breasted3 from '../assets/breasted3.jpeg'
+import breasted4 from '../assets/breasted4.jpeg'
+
+import bucket1 from '../assets/bucket1.jpg'
+import bucket2 from '../assets/bucket2.jpg'
+import bucket3 from '../assets/bucket3.jpg'
+import bucket4 from '../assets/bucket4.jpg'
+
+import burger1 from '../assets/burger1.jpeg'
+import burger2 from '../assets/burger2.jpeg'
+import burger3 from '../assets/burger3.jpeg'
+import burger4 from '../assets/burger4.jpeg'
+
+import buttons1 from '../assets/buttons1.jpg'
+import buttons2 from '../assets/buttons2.jpg'
+import buttons3 from '../assets/buttons3.jpg'
+import buttons4 from '../assets/buttons4.jpg'
+
+import cardigan1 from '../assets/cardigan1.jpg'
+import cardigan2 from '../assets/cardigan2.jpg'
+import cardigan3 from '../assets/cardigan3.jpg'
+import cardigan4 from '../assets/cardigan4.jpg'
+
+import carpenter1 from '../assets/carpenter1.jpg'
+import carpenter2 from '../assets/carpenter2.jpg'
+import carpenter3 from '../assets/carpenter3.jpg'
+import carpenter4 from '../assets/carpenter4.jpg'
+
+import chinos1 from '../assets/chinos1.jpeg'
+import chinos2 from '../assets/chinos2.jpeg'
+import chinos3 from '../assets/chinos3.jpeg'
+import chinos4 from '../assets/chinos4.jpeg'
+
+import colored1 from '../assets/colored1.jpg'
+import colored2 from '../assets/colored2.jpg'
+import colored3 from '../assets/colored3.jpg'
+import colored4 from '../assets/colored4.jpg'
+
+import corduroy1 from '../assets/corduroy1.jpeg'
+import corduroy2 from '../assets/corduroy2.jpeg'
+import corduroy3 from '../assets/corduroy3.jpeg'
+import corduroy4 from '../assets/corduroy4.jpeg'
+
+import corset1 from '../assets/corset1.jpeg'
+import corset2 from '../assets/corset2.jpeg'
+import corset3 from '../assets/corset3.jpeg'
+import corset4 from '../assets/corset4.jpeg'
+
+import cropped1 from '../assets/cropped1.jpeg'
+import cropped2 from '../assets/cropped2.jpeg'
+import cropped3 from '../assets/cropped3.jpeg'
+import cropped4 from '../assets/cropped4.jpeg'
+
+import cup1 from '../assets/cup1.jpeg'
+import cup2 from '../assets/cup2.jpeg'
+import cup3 from '../assets/cup3.jpeg'
+import cup4 from '../assets/cup4.jpeg'
+
+import cutout1 from '../assets/cutout1.jpeg'
+import cutout2 from '../assets/cutout2.jpeg'
+import cutout3 from '../assets/cutout3.jpeg'
+import cutout4 from '../assets/cutout4.jpeg'
+
+import denim1 from '../assets/denim1.jpg'
+import denim2 from '../assets/denim2.jpg'
+import denim3 from '../assets/denim3.jpg'
+import denim4 from '../assets/denim4.jpg'
+
+import dress1 from '../assets/dress1.jpg'
+import dress2 from '../assets/dress2.jpg'
+import dress3 from '../assets/dress3.jpg'
+import dress4 from '../assets/dress4.jpg'
+
+import blackdress1 from '../assets/dressblack1.jpeg'
+import blackdress2 from '../assets/dressblack2.jpeg'
+import blackdress3 from '../assets/dressblack3.jpeg'
+import blackdress4 from '../assets/dressblack4.jpeg'
+
+import faux1 from '../assets/faux1.jpg'
+import faux2 from '../assets/faux2.jpg'
+import faux3 from '../assets/faux3.jpg'
+import faux4 from '../assets/faux4.jpg'
+
+import fauxx1 from '../assets/faux1-1.jpg'
+import fauxx2 from '../assets/faux1-2.jpg'
+import fauxx3 from '../assets/faux1-3.jpg'
+import fauxx4 from '../assets/faux1-4.jpg'
+
+import flared1 from '../assets/flared1.jpg'
+import flared2 from '../assets/flared2.jpg'
+import flared3 from '../assets/flared3.jpg'
+import flared4 from '../assets/flared4.jpg'
+
+import flufy1 from '../assets/flufy.jpeg'
+import flufy2 from '../assets/flufy2.jpeg'
+import flufy3 from '../assets/flufy3.jpeg'
+
+import fuzzy1 from '../assets/fuzzy1.jpg'
+import fuzzy2 from '../assets/fuzzy2.jpg'
+import fuzzy3 from '../assets/fuzzy3.jpg'
+import fuzzy4 from '../assets/fuzzy4.jpg'
+
+import graphic1 from '../assets/graphic1.jpg'
+import graphic2 from '../assets/graphic2.jpg'
+import graphic3 from '../assets/graphic3.jpg'
+import graphic4 from '../assets/graphic4.jpg'
+
+import groovy1 from '../assets/groovy1.jpg'
+import groovy2 from '../assets/groovy2.jpg'
+import groovy3 from '../assets/groovy3.jpg'
+import groovy4 from '../assets/groovy4.jpg'
+
+import hoodie1 from '../assets/hoodie1.jpeg'
+import hoodie2 from '../assets/hoodie2.jpeg'
+import hoodie3 from '../assets/hoodie3.jpeg'
+import hoodie4 from '../assets/hoodie4.jpeg'
+
+import jacket1 from '../assets/jacket1.jpg'
+import jacket2 from '../assets/jacket2.jpg'
+import jacket3 from '../assets/jacket3.jpg'
+import jacket4 from '../assets/jacket4.jpg'
+
+import knotted1 from '../assets/knotted1.jpeg'
+import knotted2 from '../assets/knotted2.jpeg'
+import knotted3 from '../assets/knotted3.jpeg'
+import knotted4 from '../assets/knotted4.jpeg'
+
+import leather1 from '../assets/leather.jpeg'
+import leather2 from '../assets/leather2.jpeg'
+import leather3 from '../assets/leather3.jpeg'
+import leather4 from '../assets/leather4.jpeg'
+
+import leggins1 from '../assets/leggings1.jpeg'
+import leggins2 from '../assets/leggings2.jpeg'
+import leggins3 from '../assets/leggings3.jpeg'
+import leggins4 from '../assets/leggings4.jpeg'
+
+import linen1 from '../assets/linen1.jpg'
+import linen2 from '../assets/linen2.jpg'
+import linen3 from '../assets/linen3.jpg'
+import linen4 from '../assets/linen4.jpg'
+
+import lines1 from '../assets/lines1.jpg'
+import lines2 from '../assets/lines2.jpg'
+import lines3 from '../assets/lines3.jpg'
+import lines4 from '../assets/lines4.jpg'
+
+import loose1 from '../assets/loose1.jpg'
+import loose2 from '../assets/loose2.jpg'
+import loose3 from '../assets/loose3.jpg'
+import loose4 from '../assets/loose4.jpg'
+
+import mom1 from '../assets/mom1.jpeg'
+import mom2 from '../assets/mom2.jpeg' 
+import mom3 from '../assets/mom3.jpeg' 
+import mom4 from '../assets/mom4.jpeg' 
+
+import mountain1 from '../assets/mountain1.jpeg'
+import mountain2 from '../assets/mountain2.jpeg'
+import mountain3 from '../assets/mountain3.jpeg'
+import mountain4 from '../assets/mountain4.jpeg'
+
+import naruto1 from '../assets/naruto1.jpeg'
+import naruto2 from '../assets/naruto2.jpeg'
+import naruto3 from '../assets/naruto3.jpeg'
+import naruto4 from '../assets/naruto4.jpeg'
+
+import nylon1 from '../assets/nylon1.jpeg'
+import nylon2 from '../assets/nylon2.jpeg'
+import nylon3 from '../assets/nylon3.jpeg'
+import nylon4 from '../assets/nylon4.jpeg'
+
+import nilon1 from '../assets/nylon1-1.jpg'
+import nilon2 from '../assets/nylon2-2.jpg'
+import nilon3 from '../assets/nylon3-3.jpg'
+import nilon4 from '../assets/nylon4-4.jpg'
+
+import overshirt1 from '../assets/overshirt1.jpg'
+import overshirt2 from '../assets/overshirt2.jpg'
+import overshirt3 from '../assets/overshirt3.jpg'
+import overshirt4 from '../assets/overshirt4.jpg'
+
+import plush1 from '../assets/plush1.jpg'
+import plush2 from '../assets/plush2.jpg'
+import plush3 from '../assets/plush3.jpg'
+import plush4 from '../assets/plush4.jpg'
+
+import pocket1 from '../assets/pucket1.jpeg'
+import pocket2 from '../assets/pucket2.jpeg'
+import pocket3 from '../assets/pucket3.jpeg'
+import pocket4 from '../assets/pucket4.jpeg'
+
+import pocketmen1 from '../assets/pucketmen1.jpeg'
+import pocketmen2 from '../assets/pucketmen2.jpeg'
+import pocketmen3 from '../assets/pucketmen3.jpeg'
+import pocketmen4 from '../assets/pucketmen4.jpeg'
+
+import printedh1 from '../assets/printedh1.jpg'
+import printedh2 from '../assets/printedh2.jpg'
+import printedh3 from '../assets/printedh3.jpg'
+import printedh4 from '../assets/printedh4.jpg'
+
+import ribbed1 from '../assets/ribbed1.jpg'
+import ribbed2 from '../assets/ribbed2.jpg'
+import ribbed3 from '../assets/ribbed3.jpg'
+import ribbed4 from '../assets/ribbed4.jpg'
+
+import ripped1 from '../assets/ripped1.jpeg'
+import ripped2 from '../assets/ripped2.jpeg'
+import ripped3 from '../assets/ripped3.jpeg'
+import ripped4 from '../assets/ripped4.jpeg'
+
+import rise1 from '../assets/rise1.jpg'
+import rise2 from '../assets/rise2.jpg'
+import rise3 from '../assets/rise3.jpg'
+import rise4 from '../assets/rise4.jpg'
+
+import shirt1 from '../assets/shirt1.jpeg'
+import shirt2 from '../assets/shirt2.jpeg'
+import shirt3 from '../assets/shirt3.jpeg'
+import shirt4 from '../assets/shirt4.jpeg'
+
+import shoulder1 from '../assets/shoulder1.jpeg'
+import shoulder2 from '../assets/shoulder2.jpeg'
+import shoulder3 from '../assets/shoulder3.jpeg'
+import shoulder4 from '../assets/shoulder4.jpeg'
+
+import sleev1 from '../assets/sleev1.jpeg'
+import sleev2 from '../assets/sleev2.jpeg'
+import sleev3 from '../assets/sleev3.jpeg'
+import sleev4 from '../assets/sleev4.jpeg'
+
+import sleeve1 from '../assets/sleeve1.jpg'
+import sleeve2 from '../assets/sleeve2.jpg'
+import sleeve3 from '../assets/sleeve3.jpg'
+import sleeve4 from '../assets/sleeve4.jpg'
+
+import slogan1 from '../assets/slogan1.jpg'
+import slogan2 from '../assets/slogan2.jpg'
+import slogan3 from '../assets/slogan3.jpg'
+import slogan4 from '../assets/slogan4.jpg'
+
+import softbag1 from '../assets/softbag1.jpeg'
+import softbag2 from '../assets/softbag2.jpeg'
+import softbag3 from '../assets/softbag3.jpeg'
+import softbag4 from '../assets/softbag4.jpeg'
+
+import starter1 from '../assets/starter1.jpeg'
+import starter2 from '../assets/starter2.jpeg'
+import starter3 from '../assets/starter3.jpeg'
+import starter4 from '../assets/starter4.jpeg'
+
+import stuff1 from '../assets/stuff1.jpeg'
+import stuff2 from '../assets/stuff2.jpeg'
+import stuff3 from '../assets/stuff3.jpeg'
+import stuff4 from '../assets/stuff4.jpeg'
+
+import suede1 from '../assets/suede1.jpg'
+import suede2 from '../assets/suede2.jpg'
+import suede3 from '../assets/suede3.jpg'
+import suede4 from '../assets/suede4.jpg'
+
+import sweat1 from '../assets/sweat1.jpeg'
+import sweat2 from '../assets/sweat2.jpeg'
+import sweat3 from '../assets/sweat3.jpeg'
+import sweat4 from '../assets/sweat4.jpeg'
+
+import sweater1 from '../assets/sweater1.jpg'
+import sweater2 from '../assets/sweater2.jpg'
+import sweater3 from '../assets/sweater3.jpg'
+import sweater4 from '../assets/sweater4.jpg'
+
+import trousers1 from '../assets/trousers1.jpeg'
+import trousers2 from '../assets/trousers2.jpeg'
+import trousers3 from '../assets/trousers3.jpeg'
+import trousers4 from '../assets/trousers4.jpeg'
+
+import tupac1 from '../assets/tupac1.jpeg'
+import tupac2 from '../assets/tupac2.jpeg'
+import tupac3 from '../assets/tupac3.jpeg'
+import tupac4 from '../assets/tupac4.jpeg'
+
+import varsity1 from '../assets/varsity1.jpeg'
+import varsity2 from '../assets/varsity2.jpeg'
+import varsity3 from '../assets/varsity3.jpeg'
+import varsity4 from '../assets/varsity4.jpeg'
+
+import vneck1 from '../assets/vneck1.jpeg'
+import vneck2 from '../assets/vneck2.jpeg'
+import vneck3 from '../assets/vneck3.jpeg'
+import vneck4 from '../assets/vneck4.jpeg'
+
+import wideleg1 from '../assets/wideleg1.jpg'
+import wideleg2 from '../assets/wideleg2.jpg'
+import wideleg3 from '../assets/wideleg3.jpg'
+import wideleg4 from '../assets/wideleg4.jpg'
+
+import zipped1 from '../assets/zipped1.jpeg'
+import zipped2 from '../assets/zipped2.jpeg'
+import zipped3 from '../assets/zipped3.jpeg'
+import zipped4 from '../assets/zipped4.jpeg'
+
+import zipper1 from '../assets/zipper1.jpeg'
+import zipper2 from '../assets/zipper2.jpeg'
+import zipper3 from '../assets/zipper3.jpeg'
+import zipper4 from '../assets/zipper4.jpeg'
