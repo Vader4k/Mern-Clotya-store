e799ddd15b01aea3f2429b00ac72539e561780b8
danie odukwe <kingdanny295@gmail.com>
2024-06-23 21:19:12 +0100

    @add/ successfully created products schema

--- server/Models/productModel.js
+++ server/Models/productModel.js
@@ -1,7 +1,36 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+
+const categorySchema = new Schema({
+    _id: { type: Schema.Types.ObjectId, auto: true },
+    name: { type: String, required: true },
+})
+
+const colorSchema = new Schema({
+    _id: { type: Schema.Types.ObjectId, auto: true },,
+    name: { type: String, required: true },
+})
+
+const sizeSchema = new Schema({
+    _id: { type: Schema.Types.ObjectId, auto: true },,
+    name: { type: String, required: true },
+})
 
 const productSchema = new mongoose.Schema({
-
+    id: { type: Number, required: true, unique: true },
+    name: { type: String, required: true },
+    old_price: { type: String, required: true },
+    new_price: { type: String, required: true },
+    reviews: { type: String, required: true },
+    img1: { type: String, required: true },
+    img2: { type: String, required: true },
+    img3: { type: String, required: true },
+    img4: { type: String, required: true },
+    tag: { type: String, required: true },
+    category: [categorySchema],
+    info: { type: String, required: true },
+    colors: [colorSchema],
+    size: [sizeSchema],
+    SKU: { type: String, required: true, }
 })
 
 export default mongoose.model('productSchema', productSchema)
