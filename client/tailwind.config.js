5780cd7b22caeb155e0ef30999249e789600b561
danie odukwe <kingdanny295@gmail.com>
2024-06-23 21:53:39 +0100

    @add/ added tailwind configs

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./index.css","./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // screens: {
+      //   xxs:"100px",
+      //   xs: "480px",
+      //   ss: "620px",
+      //   sm: "768px",
+      //   md: "1060px",
+      //   lg: "1200px",
+      //   xl: "1700px",
+      // },
+      fontFamily : {
+        poppins: ["Poppins", "sans-serif"],
+        roboto : ["Roboto", "sans-serif"]
+      },
+      wordSpacing: {
+        'tight': '-0.05em',
+        'normal': '0',
+        'wide': '1.2em',
+      },
+      colors : {
+        pink: "#fdf8f7"
+      }
+    },
   },
   plugins: [],
 }
-
