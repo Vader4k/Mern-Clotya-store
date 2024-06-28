b107d48bf9017d892ad1ec8ac839ff9c1965fbd9
danie odukwe <kingdanny295@gmail.com>
2024-06-28 16:01:02 +0100

    @add/ updated the menu bar on what ti display in shop path

--- client/src/components/Menubar.jsx
+++ client/src/components/Menubar.jsx
@@ -1,10 +1,15 @@
 import { menuLinks } from '../constants/products'
 import { Link } from 'react-router-dom'
+import { IoHomeOutline, IoFilterOutline, IoHeartOutline, IoPersonOutline, IoSearchOutline } from "react-icons/io5";
+import { useLocation } from 'react-router-dom';
 
 const Menubar = () => {
+
+  const location = useLocation()
+
   return (
     <section>
-      <ul className='w-full flex h-full items-center justify-between px-10 sm:px-28 bg-white'>
+      <ul className={`${location.pathname === '/shop' ? 'hidden' : 'flex'} w-full h-full items-center justify-between px-10 sm:px-28 bg-white`}>
         {menuLinks.map((items) =>(
           <Link to={items.url} key={items.id}>
             <div className='flex text-gray-400 justify-center items-center flex-col gap-2'>
@@ -13,6 +18,37 @@ const Menubar = () => {
             </div>
           </Link>
         ))}
+      </ul>
+
+      <ul className={`${location.pathname != '/shop' ? 'hidden' : 'flex'} w-full h-full items-center justify-between px-10 sm:px-28 bg-white`}>
+        <Link to='/'>
+          <div className='flex text-gray-400 justify-center items-center flex-col gap-2'>
+            <IoHomeOutline className='text-[1.2rem]' />
+            <p className='text-[0.65rem] font-medium uppercase'>HOME</p>
+          </div> 
+        </Link>
+        <div className='flex text-gray-400 justify-center items-center flex-col gap-2'>
+          <IoFilterOutline className='text-[1.2rem]' />
+          <p className='text-[0.65rem] font-medium uppercase'>FILTER</p>
+        </div>
+        <Link to='/search'>
+          <div className='flex text-gray-400 justify-center items-center flex-col gap-2'>
+            <IoSearchOutline className='text-[1.2rem]' />
+            <p className='text-[0.65rem] font-medium uppercase'>SEARCH</p>
+          </div>
+        </Link>
+        <Link to='/favorites'>
+          <div className='flex text-gray-400 justify-center items-center flex-col gap-2'>
+            <IoHeartOutline className='text-[1.2rem]' />
+            <p className='text-[0.65rem] font-medium uppercase'>WISHLIST</p>
+          </div>
+        </Link>
+        <Link to='/profile'>
+          <div className='flex text-gray-400 justify-center items-center flex-col gap-2'>
+            <IoPersonOutline className='text-[1.2rem]' />
+            <p className='text-[0.65rem] font-medium uppercase'>ACCOUNT</p>
+          </div>
+        </Link>
       </ul>
     </section>
   )
