ac8670cb1bfb2a8b77686320ea54fd2fcacd93e3
danie odukwe <kingdanny295@gmail.com>
2024-07-09 20:45:30 +0100

    @fix dashboard issue

--- client/src/components/UserDashboard/Dashboard.jsx
+++ client/src/components/UserDashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = ({ setTab, userData, logout }) => {
   return (
     <div>
-      <h1>Hello <span className='font-medium'>{userData.username}</span> ( not <span className='font-medium'>{userData.username}? <span onClick={logout} className='text-red-500 cursor-pointer'>Logout</span></span> )</h1>
+      <h1>Hello <span className='font-medium'>{userData?.username}</span> ( not <span className='font-medium'>{userData?.username}? <span onClick={logout} className='text-red-500 cursor-pointer'>Logout</span></span> )</h1>
 
       <p className='mt-4 text-[0.95rem]'>From you account dashboard, you can view your <span className='text-red-500 cursor-pointer' onClick={()=>setTab("orders")}>recent orders,</span> manage your <span className='text-red-500 cursor-pointer' onClick={()=>setTab("address")}>shipping and billing addresses,</span> and <span className='text-red-500 cursor-pointer' onClick={()=>setTab("details")}>edit your password and account details</span></p>
     </div>
